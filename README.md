2a54f3d83697ee53f0396b94ebd65d626be69d4f
Megha Garg <Megha_Garg@gap.com>
2021-05-17 11:39:24 -0400

    Hey Here is my first Spring MVC project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # welcome-spring-mvc
 Creating a new spring mvc which will cover all the basics of model view controller concept.
+
+
+
---
+++ src/main/java/com/learning/AddController.java
@@ -0,0 +1,16 @@
+package com.learning;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class AddController {
+
+	@RequestMapping("/add")
+	public String add(){
+		
+		System.out.println("Hey Spring MVC");
+		return "display.jsp";
+	}
+}
